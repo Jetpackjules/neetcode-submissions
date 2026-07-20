d5351f16f27a279b87ed8ed209747487c9a153b5
Jules Ropars <46934175+Jetpackjules@users.noreply.github.com>
2026-07-20 16:27:19 -0700

    Add: two-integer-sum - submission-3

---
+++ Data Structures & Algorithms/two-integer-sum/submission-3.py
@@ -0,0 +1,13 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        pairs = {}
+
+        for idx, num in enumerate(nums):
+            diff = target-num
+            if diff in pairs:
+                return [pairs[diff], idx]
+            else:
+                pairs[num] = idx
+
+        # safe fail case
+        return []
