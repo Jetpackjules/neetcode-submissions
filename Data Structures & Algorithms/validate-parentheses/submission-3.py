f418d933c5f90a1ed247d23a20af1bf40818cd0e
Jules Ropars <46934175+Jetpackjules@users.noreply.github.com>
2026-07-23 16:25:11 -0700

    Add: validate-parentheses - submission-3

---
+++ Data Structures & Algorithms/validate-parentheses/submission-3.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        guide = {')': '(', ']': '[', '}': '{'}
+
+        for char in s:
+            # print(stack, char)
+            if char not in guide:
+                stack.append(char)
+            else:
+                if not stack or stack.pop() != guide[char]:
+                    return False
+        return True if stack == [] else False
+        
