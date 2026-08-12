74477eec582b5b50fe727b8833cc65aadb8debc8
Jules Ropars <46934175+Jetpackjules@users.noreply.github.com>
2026-08-12 14:51:23 -0700

    Add: house-robber - submission-5

---
+++ Data Structures & Algorithms/house-robber/submission-5.py
@@ -0,0 +1,13 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        #  DP: two most recent houses, can only rob one! 
+        # rob, not rob, not rob, rob
+        # two states: if I DID, and DIDNT rob the alst house!
+        # [did not rob, did rob] -> 
+        # edge: empty,. or one house
+
+        last2 = [0,0]
+        for house in nums:
+            last2 = [last2[1], max(last2[1], last2[0]+house)]
+        
+        return max(last2)
